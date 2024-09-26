2d6bdaea29e871516634bc7ff7c4f93af92f03c1
Amit Tiwari <138901423+i-amit-tiwari@users.noreply.github.com>
2024-09-26 01:55:35 +0000

    variable const

---
+++ Basics/varConst.js
@@ -0,0 +1,7 @@
+const sId = "CSE22/017";
+let sName = "Amit Tiwari";
+let sRollNo;
+// it is a single line comment
+/*it is a multi line comment*/
+console.log(sRollNo);
+console.table([sId,sName]);
