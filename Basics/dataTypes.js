380ce9061beb1027a16accdd3681918c9f98e4f1
Amit Tiwari <138901423+i-amit-tiwari@users.noreply.github.com>
2024-09-26 01:56:01 +0000

    dataTypes

---
+++ Basics/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";// treat all javascript code as newer version
+
+//alert("hello");
+//alert(3+3); we are using node js not browser
+
+// maintain code redability cleaniness
+
+// for documentation refer-->mdn or ecma
+
+let name = "amit";//string
+let age = 20;// integer, also have "bigint"
+let isLoggedIn = false;//boolean
+//null => standalone value 
+//undefined =>value can be there, right now not assigned
+//symbol => provides uniqueness
+
+//object
+
+console.log(typeof "Amit")
+console.log(typeof null) // o/p => object
+console.log(typeof undefined)// o/p => undefined
