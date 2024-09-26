e464d6429536c5c223ae0059be92734f8cc48e20
Amit Tiwari <138901423+i-amit-tiwari@users.noreply.github.com>
2024-09-26 02:13:29 +0000

    typeConversion

---
+++ Basics/conversionOperations.js
@@ -0,0 +1,28 @@
+// let score = "33";
+// let score1 = "33ab";
+// console.log(typeof score);
+// console.log(typeof(score));
+// console.log(typeof(score1));
+
+// let valueInNumber = Number(score)
+// let valueInNumber1 = Number(score1)
+// console.log(typeof(valueInNumber));
+// console.log(typeof(valueInNumber1));
+// console.log(valueInNumber);
+// console.log(valueInNumber1);
+
+// 33=> 33
+//"33"=> NaN
+//true => 1; false=>0
+
+// let isLoggedIn =1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let isLoggedIn =0
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let isLoggedIn ="" //will give false
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
